bf53ec98b0513200c4e5b3049af3d4c5f76636f2
JMK100 <114542491+JMK1002@users.noreply.github.com>
2026-01-16 10:35:33 -0500

    replaced the template init code with my old code

--- ags/app.ts
+++ ags/app.ts
@@ -1,19 +1,21 @@
-import { App } from "astal/gtk3"
+import app from "ags/gtk3/app"
+import Gtk from "gi://Gtk?version=3.0"
 import style from "./style.scss"
+import { timeout } from "./types/utils/timeout"
+
 import Bar from "./widgets/Bar/Bar"
 import Calendar from "./widgets/Calendar/Calendar"
 import TextDisplay from "./widgets/TextDisplay/TextDisplay"
 import Quicksettings from "./widgets/Quicksettings/Quicksettings"
 import Time from "./widgets/Time/Time"
 import WallpaperChanger from "./widgets/WallpaperChanger/WallpaperChanger"
-import { timeout } from "astal"
 
-App.start({
+app.start({
     css: style,
     main() {
-        App.apply_css("./wallpaper.css")
-        App.get_monitors().map(Bar)
-        App.get_monitors().map(Calendar)
+        app.apply_css("./wallpaper.css")
+        app.get_monitors().map(Bar)
+        app.get_monitors().map(Calendar)
         // App.get_monitors().map(Quicksettings)
         // App.get_monitors().map(TextDisplay)
     },
