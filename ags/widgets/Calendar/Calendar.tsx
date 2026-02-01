d53a2d4d6ba43d9b4db1063f17e2e3f56cad3a58
JMK100 <114542491+JMK1002@users.noreply.github.com>
2026-02-01 13:23:34 -0500

    updated calendar and time widgets to use refactored WindowRevealer

--- ags/widgets/Calendar/Calendar.tsx
+++ ags/widgets/Calendar/Calendar.tsx
@@ -1,13 +1,25 @@
 import { Astal, Gdk, Gtk } from "ags/gtk4"
 import WindowRevealer from "../../utils/WindowRevealer"
+import GoogleCalendar from "./components/GoogleCalendar"
+import EventDisplay from "./components/EventDisplay"
+import ICAL from "ical.js"
+import { interval } from "ags/time"
+import app from "ags/gtk4/app"
 
+const myCal = new GoogleCalendar(`jmkovalovsky@gmail.com.ics`)
 
-export default function CalendarWindow(gdkmonitor: Gdk.Monitor) {
-
-	return WindowRevealer({
-		name:"CalendarWindow",
-		className: "transparentBackground darkerBackground",
-		child: <box>
+export default (gdkmonitor: Gdk.Monitor) => {
+	const window = <window
+		name={"CalendarWindow"}
+		application={app}
+		class={"transparentBackground darkerBackground"}
+		anchor={Astal.WindowAnchor.TOP}
+		layer={Astal.Layer.TOP}
+		keymode={Astal.Keymode.ON_DEMAND}
+		exclusivity={Astal.Exclusivity.NORMAL}
+		visible={true}
+		>
+		<box>
 			<Gtk.Calendar class="calendar"/>
 			<box orientation={Gtk.Orientation.VERTICAL}>
 				<box>
@@ -20,15 +32,11 @@ export default function CalendarWindow(gdkmonitor: Gdk.Monitor) {
 						label={"Reload Events"}
 					/>
 				</box>
-				{/* {getMenu()} */}
 			</box>
-		</box>,
-		anchor:Astal.WindowAnchor.TOP,
-		layer:Astal.Layer.TOP,
-		keymode:Astal.Keymode.ON_DEMAND,
-		exclusivity:Astal.Exclusivity.NORMAL,
-		visible: false,
-		css: "",
-		$: () => {},
-	})
+		</box>
+	</window>
+	
+	if (window instanceof Astal.Window) {
+		return WindowRevealer(window)
+	}
 }
