9cf256e386c37c17085139c7db7b20b643b172a0
JMK100 <114542491+JMK1002@users.noreply.github.com>
2026-04-30 21:33:36 -0400

    made minor changes and added tabliss file

--- ags/scripts/wall/change.sh
+++ ags/scripts/wall/change.sh
@@ -5,6 +5,7 @@ bash scripts/wall/wall.sh "wallpapers/$1"
 wal -i $(awww query | grep -o -E 'image: .+' | tail -c+8) -n --cols16
 
 echo hyprctl clients | grep "Spotify"
+bash scripts/wall/Chromium/generate-theme.sh
 
 bash /home/Julian/.config/kitty/reload-theme.sh
 xrdb $HOME/.cache/wal/colors.Xresources
